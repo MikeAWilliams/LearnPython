f8387d4989ed35f9c31642d8a8d905fd21f168e3
Mike Williams <mumakin@gmail.com>
2020-05-06 23:55:01 -0600

    solve bowling kata for first time

---
+++ bowlingKata01/bowling.py
@@ -0,0 +1,50 @@
+def DumbScoreFrame(frame):
+    if 2 < len(frame):
+        raise Exception("the frame is to long")
+    result = 0
+    for attempt in frame:
+        result+=attempt
+    return result
+
+def IsStrike(frame):
+    return 10 == frame[0]
+
+def IsSpare(frame):
+    if not IsStrike(frame):
+        return 10 == DumbScoreFrame(frame)
+    return False
+
+def ScoreFrame(thisFrame, nextFrame, thirdFrame):
+    if IsSpare(thisFrame):
+        return 10 + nextFrame[0]
+    if IsStrike(thisFrame):
+        if 10 == nextFrame[0]:
+            return 20 + thirdFrame[0]
+        return 10 + nextFrame[0] + nextFrame[1]
+    return DumbScoreFrame(thisFrame)
+
+def ScoreFrameByIndex(index, frames):
+    nextIndex = index + 1
+    nextFrame = [0,0]
+    thirdFrame = [0,0]
+    thisFrame = frames[index]
+
+    if nextIndex < len(frames):
+        nextFrame = frames[nextIndex]
+        thirdFrame = nextFrame
+    else:
+        nextFrame[0] = thisFrame[1]
+
+    thirdIndex = nextIndex + 1
+    if thirdIndex < len(frames):
+        thirdFrame = frames[thirdIndex]
+    else:
+        if 3 == len(thisFrame):
+            thirdFrame[0] = thisFrame[2]
+    return ScoreFrame(thisFrame, nextFrame, thirdFrame)
+
+def BowlingScore(frames):
+    result = 0
+    for i in range(10):
+        result += ScoreFrameByIndex(i, frames)
+    return result
---
+++ bowlingKata01/test.py
@@ -0,0 +1,45 @@
+
+from __future__ import division
+
+import unittest
+import pytest
+import bowling
+
+class BowlingTest(unittest.TestCase):
+
+    def test_no_strike_no_spare(self):
+        frames = [[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1]]
+        self.assertEqual(20, bowling.BowlingScore(frames))
+
+    def test_all_spare_first_nine(self):
+        frames = [[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 0]]
+        self.assertEqual(9*19 + 9, bowling.BowlingScore(frames))
+
+    def test_all_spare(self):
+        frames = [[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[9, 1],[5]]
+        self.assertEqual(9*19 + 15, bowling.BowlingScore(frames))
+
+    def test_all_strike(self):
+        frames = [[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[9, 0]]
+        self.assertEqual(7*30 + 29 + 19 + 9, bowling.BowlingScore(frames))
+
+    def test_alternate_strike_spare(self):
+        frames = [[10, 0],[9, 1],[10, 0],[9, 1],[10, 0],[9, 1],[10, 0],[9, 1],[10, 0],[9, 0]]
+        self.assertEqual(4 * 20 + 19 + 4 * 20 + 9, bowling.BowlingScore(frames))
+
+    def test_three_frame_not_last(self):
+        with pytest.raises(Exception):
+            frames = [[1, 1],[1, 1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1]]
+            assert bowling.BowlingScore(frames)
+
+    def test_three_frame_second_to_last(self):
+        with pytest.raises(Exception):
+            frames = [[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1, 1],[1, 1]]
+            assert bowling.BowlingScore(frames)
+
+    def test_perfrect_game(self):
+        frames = [[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 10, 10]]
+        self.assertEqual(300, bowling.BowlingScore(frames))
+
+if __name__ == '__main__':
+    unittest.main()
