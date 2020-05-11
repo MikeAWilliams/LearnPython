56fa39c9c3281a3b14f1899361f5f732f159032a
Michael Williams <mwilliams@alteryx.com>
2020-05-11 10:42:20 -0600

    reformat

--- bowlingKata02/bowling.py
+++ bowlingKata02/bowling.py
@@ -1,22 +1,28 @@
 def IsStrike(frame):
     return 10 == frame[0]
 
+
 def IsSpare(frame):
     return 10 == SumFrame(frame) and 10 != frame[0]
+
 
 def ScoreStrikeFramesZeroToSeven(nextFrame, thirdFrame):
     if IsStrike(nextFrame):
         return 20 + thirdFrame[0]
     return 10 + nextFrame[0] + nextFrame[1]
 
+
 def ScoreStrikeFrameEight(frameNine, notUsed):
     return 10 + frameNine[0] + frameNine[1]
+
 
 def ScoreStrikeFrameNine(frameNine):
     return SumFrame(frameNine)
 
+
 def ScoreSpareFramesZeroToEight(nextFrame):
     return 10 + nextFrame[0]
+
 
 def SumFrame(frame):
     result = 0
@@ -24,28 +30,39 @@ def SumFrame(frame):
         result += bowl
     return result
 
-def ScoreFrameByFunction(thisFrame, nextFrame, thirdFrame, strikeFunction, spareFunction):
+
+def ScoreFrameByFunction(thisFrame, nextFrame, thirdFrame, strikeFunction,
+                         spareFunction):
     if IsStrike(thisFrame):
         return strikeFunction(nextFrame, thirdFrame)
     if IsSpare(thisFrame):
         return spareFunction(nextFrame)
     return SumFrame(thisFrame)
 
+
 def ScoreFramesZeroToSeven(thisFrame, nextFrame, thirdFrame):
-    return ScoreFrameByFunction(thisFrame, nextFrame, thirdFrame, ScoreStrikeFramesZeroToSeven, ScoreSpareFramesZeroToEight)
+    return ScoreFrameByFunction(thisFrame, nextFrame, thirdFrame,
+                                ScoreStrikeFramesZeroToSeven,
+                                ScoreSpareFramesZeroToEight)
+
 
 def ScoreFrameEight(frameEight, frameNine):
-    return ScoreFrameByFunction(frameEight, frameNine, [0,0], ScoreStrikeFrameEight, ScoreSpareFramesZeroToEight)
+    return ScoreFrameByFunction(frameEight, frameNine, [0, 0],
+                                ScoreStrikeFrameEight,
+                                ScoreSpareFramesZeroToEight)
+
 
 def ScoreFrameNine(frameNine):
     if IsStrike(frameNine):
         return ScoreStrikeFrameNine(frameNine)
     return SumFrame(frameNine)
 
+
 def CalculateScore(frames):
     result = 0
     for index in range(8):
-        result += ScoreFramesZeroToSeven(frames[index], frames[index + 1], frames[index + 2])
+        result += ScoreFramesZeroToSeven(frames[index],
+                                         frames[index + 1], frames[index + 2])
 
     result += ScoreFrameEight(frames[8], frames[9])
     result += ScoreFrameNine(frames[9])
--- bowlingKata02/test.py
+++ bowlingKata02/test.py
@@ -1,31 +1,39 @@
 import unittest
 import bowling
 
+
 class BowlingTest(unittest.TestCase):
 
     def test_no_strike_no_spare(self):
-        frames = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1,0]]
+        frames = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1],
+                  [1, 1], [1, 1], [1, 1], [1, 1], [1, 1, 0]]
         self.assertEqual(20, bowling.CalculateScore(frames))
 
     def test_strikes_no_spare_no_consecutive_strikes(self):
-        frames = [[10,0],[1,1],[10,0],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1,0]]
+        frames = [[10, 0], [1, 1], [10, 0], [1, 1], [1, 1],
+                  [1, 1], [1, 1], [1, 1], [1, 1], [1, 1, 0]]
         self.assertEqual(16 + 20 + 4, bowling.CalculateScore(frames))
 
     def test_strikes_no_spare_consecutive_strikes(self):
-        frames = [[10,0],[10,0],[10,0],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1,0]]
+        frames = [[10, 0], [10, 0], [10, 0], [1, 1], [1, 1],
+                  [1, 1], [1, 1], [1, 1], [1, 1], [1, 1, 0]]
         self.assertEqual(14 + 30 + 21 + 12, bowling.CalculateScore(frames))
 
     def test_perfect_game(self):
-        frames = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]]
+        frames = [[10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+                  [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]]
         self.assertEqual(300, bowling.CalculateScore(frames))
 
     def test_spare_no_strike(self):
-        frames = [[9,1],[1,1],[9,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1,0]]
+        frames = [[9, 1], [1, 1], [9, 1], [1, 1], [1, 1],
+                  [1, 1], [1, 1], [1, 1], [1, 1], [1, 1, 0]]
         self.assertEqual(16 + 20 + 2, bowling.CalculateScore(frames))
 
     def test_all_spare(self):
-        frames = [[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1,9]]
+        frames = [[9, 1], [9, 1], [9, 1], [9, 1], [9, 1],
+                  [9, 1], [9, 1], [9, 1], [9, 1], [9, 1, 9]]
         self.assertEqual(190, bowling.CalculateScore(frames))
+
 
 if __name__ == '__main__':
     unittest.main()
